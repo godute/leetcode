145baaf120fcc767216faa6f9ffdedeeacd57a37
Jeong Woo Tae <wjddnxo1478@naver.com>
2021-08-22 20:40:27 +0900

    problem 1 좋은 방법

--- src/problem_1/Solution.kt
+++ src/problem_1/Solution.kt
@@ -6,15 +6,11 @@ Output: [0, 1] Because nums[0] + nums[1] == 9, we return [0, 1]
  */
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val result = intArrayOf()
-
-        for (i in 0 until nums.lastIndex) {
-            for(j in i+1..nums.lastIndex){
-                if(nums[i] + nums[j] == target) {
-                    return intArrayOf(i, j)
-                }
-            }
+        val map = mutableMapOf<Int, Int>()
+        nums.forEachIndexed { index, int ->
+            map[int]?.let { return intArrayOf(it, index) }
+            map[target - int] = index
         }
-        return result
+        return intArrayOf()
     }
 }
